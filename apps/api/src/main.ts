e72999b74359affbfe236108eab0378e0cb0a7ab
Robert Garcia <rbert.g.ch@gmail.com>
2024-06-02 13:23:05 -0400

    Update Template Delete Docs

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -35,43 +35,6 @@ async function bootstrap() {
     port = configService.get<string>('PORT', { infer: true });
   }
 
-  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
-
-  const options = new DocumentBuilder()
-    .setTitle('Generic API')
-    .setDescription('A generic API')
-    .setVersion('1.0')
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-        name: 'JWT',
-        description: 'Enter JWT token',
-        in: 'header',
-      },
-      sJwtBearer,
-    )
-    .addApiKey(
-      {
-        type: 'apiKey',
-        scheme: 'apiKey',
-        bearerFormat: 'apiKey',
-        name: sApiKeyBearer,
-        description: 'Enter API key',
-        in: 'header',
-      },
-      sApiKeyBearer,
-    )
-    .build();
-
-  const document = SwaggerModule.createDocument(app, options);
-
-  // Set up Swagger UI
-  SwaggerModule.setup(`docs`, app, document);
-
-  logger.log(`Swagger UI available at /docs`);
-
   await app.listen(port, '0.0.0.0');
 
   logger.log(`Listening on port ${port}`);
